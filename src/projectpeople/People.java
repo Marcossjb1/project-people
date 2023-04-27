238441d600569a6243a2e808ac1475a404a80f9b
unknown <ruchellguilherme100@gmail.com>
2023-04-26 22:53:37 -0300

    add comments

--- src/projectpeople/People.java
+++ src/projectpeople/People.java
@@ -1,4 +1,7 @@
 package src.projectpeople;
 
 public class People {
+
+  //Todo: create properties for class people: name, age, nacionality, cpf, address
+  //Todo: should create getters and setters
 }
