fba19f94d89120f55c13587a30fba4ece891452d
unknown <ruchellguilherme100@gmail.com>
2023-05-02 22:55:43 -0300

    implementing methods validator cpf and cnpj

--- src/projectpeople/NaturalPerson.java
+++ src/projectpeople/NaturalPerson.java
@@ -1,5 +1,22 @@
 package src.projectpeople;
 
 public class NaturalPerson extends People {
+
     public String cpf;
+
+    public String validatorCpf(String cnpj){
+        if (cnpj.length() != 14){
+            return  "CPF invalid";
+        }else {
+            return  "CPF valid";
+        }
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
